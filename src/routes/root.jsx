0b98b89f04aa3ff1ab079ecb7eeb28ffa67dbe79
Arseny Prosvetov <arsprosvet@gmail.com>
2024-07-22 17:50:06 +0300

    useSubmit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,8 +5,10 @@ import {
   Form,
   redirect,
   useNavigation,
+  useSubmit,
 } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
+import { useEffect } from 'react';
 
 export async function action() {
   const contact = await createContact();
@@ -23,6 +25,12 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    document.getElementById('q').value = q;
+  }, [q]);
+
   return (
     <>
       <div id="sidebar">
@@ -36,6 +44,9 @@ export default function Root() {
               type="search"
               name="q"
               defaultValue={q}
+              onChange={event => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
